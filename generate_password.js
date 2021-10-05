5d8b93dcdff338e91800848ee25b8be030cd8aa7
Xavier-Hsiao <tim88776@gmail.com>
2021-10-05 22:37:26 +0800

    Complete generate_password function
    1. Complete the logic: remove what users might want to exclude
    2. Complete the final part of starting generating random password by including pickRandomCharacter function

--- generate_password.js
+++ generate_password.js
@@ -1,3 +1,10 @@
+// define a function to pick an element from collection array randomly 
+function pickRandomCharacter(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+
 // define generatePassword function
 function generatePassword() {
   // define all the elements users might use
@@ -31,14 +38,25 @@ function generatePassword() {
   if(dummyData.symbols) {
     collection = collection.concat([...symbols])
   }
-  console.log('Collection:', collection)
+
 
   // remove what users might want to exclude
+  collection = collection.filter(character => 
+    // if(dummyData.excludeCharacters.includes(character)) {
+    //   return false
+    // } 
+    // return true
+    !dummyData.excludeCharacters.includes(character)
+  )
 
   // start generating random passwords
-
+  let password = ''
+  for(let i = 0; i < dummyData.length; i++) {
+    password += pickRandomCharacter(collection)
+  }
+  
   // return the generated password
-  console.log('Boot the password generating function!')
+  return password
 }
 
 generatePassword();
